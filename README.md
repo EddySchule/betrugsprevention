06e52e6bbab7542410e806848c8da0eb592983f7
EddySchule <eduard.schneider.2008@gmail.com>
2022-01-17 15:43:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# betrugsprevention
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/betrugsprevention)
---
+++ script.js
@@ -0,0 +1,16 @@
+window.goToPage = function() {
+  const [lieferland, rechnungsland] = document.getElementsByClassName('land');
+  const [lieferstadt, rechnungsstadt] = document.getElementsByClassName('stadt');
+  const [lieferpostleitzahl, rechnungspostleitzahl] = document.getElementsByClassName('postleitzahl');
+  const [lieferhausnummer, rechnungshausnummer] = document.getElementsByClassName('hausnummer');
+  const [lieferstraße, rechnungsstraße] = document.getElementsByClassName('straße');
+  const score =
+    (lieferland.value === rechnungsland.value ? 0 : 25) +
+    (lieferstadt.value === rechnungsstadt.value ? 0 : 20) +
+    (lieferpostleitzahl.value === rechnungspostleitzahl.value ? 0 : 17) +
+    (lieferhausnummer.value === rechnungshausnummer.value ? 0 : 5) +
+    (lieferstraße.value === rechnungsstraße.value ? 0 : 10);
+  alert('Betrugswarschenlichkeit: ' + (score === 0 ? 1 : score))
+  }
+
+  
